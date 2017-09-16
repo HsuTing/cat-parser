0eb699d21564b6a072022a88c4d53391d367b7f9
HsuTing <hsuting0106@gmail.com>
2017-09-16 22:27:15 +0800

    fix bug about river

--- src/bin/core/get-river.js
+++ src/bin/core/get-river.js
@@ -1,9 +1,18 @@
 'use strict';
 
-import fetch from 'node-fetch';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import download from 'download';
 
-export default fetch('http://data.wra.gov.tw/Service/OpenData.aspx?format=json&id=336F84F7-7CFF-4084-9698-813DD1A916FE')
-  .then(res => res.json())
+export default download(
+  'http://data.wra.gov.tw/Service/OpenData.aspx?format=json&id=336F84F7-7CFF-4084-9698-813DD1A916FE',
+  {encoding: 'utf-8'}
+)
+  .then(data => {
+    fs.writeFileSync(path.resolve(process.cwd(), './data/download-river.json'), data);
+    return require(path.resolve(process.cwd(), './data/download-river.json'));
+  })
   .then(({RiverCode_OPENDATA}) => ({
     rivers: RiverCode_OPENDATA.reduce((result, {BasinName, EnglishBasinName}) => {
       result[EnglishBasinName.replace(/ /g, '')] = BasinName.replace(/ /g, '');
